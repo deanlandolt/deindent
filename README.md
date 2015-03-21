47d1f9b23647177e58468d3fc9d5fa5f3e6a4c4f
Dean Landolt <dean@deanlandolt.com>
2015-03-20 22:36:12 -0400

    Add the code

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # deindent
-ES6 template string helper for deindentation
+
+ES6 template string helper for deindentation (adapted from https://gist.github.com/zenparsing/5dffde82d9acef19e43c).
+
+```js
+var deindent = require('deindent')
+
+// as a string function
+deindent(`
+    this
+    is
+  the ${ "end" }
+      my only
+      friend
+    the end
+`);
+
+// as a template tag
+deindent`
+    this
+    is
+  the ${ "end" }
+      my only
+      friend
+    the end
+`;
+
+
+// as a higher-order template tag
+deindent(String.raw)`
+    this
+    is
+  the ${ "end" }
+      my only
+      friend
+    the end
+`;
+
+// all three should output the same deindented string
+'\nthis\nis\nthe end\n  my only\n  friend\nthe end\n'
+```
---
+++ index.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = function deindent(callSite) {
+  let args = [].slice.call(arguments, 1);
+
+  function format(str) {
+
+    let size = -1;
+
+    return str.replace(/\n(\s+)/g, function (m, m1) {
+
+      if (size < 0) {
+        size = m1.replace(/\t/g, '    ').length;
+      }
+
+      return '\n' + m1.slice(Math.min(m1.length, size));
+    });
+  }
+
+  if (typeof callSite === 'string') {
+    return format(callSite);
+  }
+
+  if (typeof callSite === 'function') {
+    return function () {
+      return format(callSite.apply(null, arguments));
+    };
+  }
+
+  let output = callSite
+    .slice(0, args.length + 1)
+    .map(function (text, i) {
+      return (i === 0 ? '' : args[i - 1]) + text
+    })
+    .join('');
+
+  return format(output);
+};
